f9c4806698cb99dc950a3a6fc3c52a1320afa384
Vitor Hainosz <vitorhainosz2@gmail.com>
2020-11-26 20:51:55 -0300

    Adicionando o mapinha das coisas

--- src/main/java/controllers/Login.java
+++ src/main/java/controllers/Login.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import utils.Alerts;
 
 public class Login extends Application {
 
@@ -24,6 +25,13 @@ public class Login extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.initStyle(StageStyle.TRANSPARENT);
 			primaryStage.show();
+			Alerts.alertAviso("Resumão professor", "Onde encontrar cada um dos pontos", ""
+					+ "Herança - Produto no processador\n\n\n"
+					+ "Agregação - Cargo no funcionario\n\n"
+					+ "Baixo nivel de acomplamento - cada classe tem a sua responsabilidade\n\n"
+					+ "Inversão de dependencia - Instancio o controller na view passando como\n parametro o DAO para o controlador(foi isso que entendi do seu audio)\n\n"
+					+ "Injeção de dependencia - Todos as classes que precisam de algo, é obrigado a passar no construtor\n\n"
+					+ "os generics estão na classe IPadraoDAO").showAndWait();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
